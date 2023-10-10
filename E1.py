87d9a05b7fae367167bf5209e2e0d0d6493aa440
franco.chavez.7e7 <franco.chavez.7e7@itb.cat>
2023-10-10 12:21:35 +0200

    E1 franco

---
+++ E1.py
@@ -0,0 +1,11 @@
+"""Franco Chavez Lava
+10/10/23
+ASIXc1C M03 UF1
+Descripcion:Demanar el diàmetre d'una pizza rodona i imprimeix la seva superfície.
+Pots usar Math.PI per escriure el valor de Pi."""
+import math
+
+diametro = int(input("¿Cual es el diametro de la pizza en centimetros?"))
+PI = math.pi
+superficie = (diametro/int(2))**int(2)*PI
+print("Tu pizza tiene una superficie de",str(superficie),"cm²")
