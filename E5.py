72c7a355f7de60f3b5ebffe7c54e33850212bb27
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-17 10:44:30 +0200

    Update

--- E5.py
+++ E5.py
@@ -2,8 +2,7 @@
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 17/10/2023
 ASIXc1C M03 UF1
-Descripció:Demana una paraula per teclat i mostrar-la per
-pantalla, canviar les vocals per als numèrics 1, 2, 3, 4 o 5.
+Descripció:Demana una paraula per teclat i mostrar-la per pantalla, canviar les vocals per als numèrics 1, 2, 3, 4 o 5.
 '''
 #Tenint en compte, que la lletra a i A és l'1, consecutivament fins a la lletra u i U que és el 5.
 def reemplaçar_vocals_amb_números(paraula):
