d6da2324ab1f7710250a02fa42209e34b0bba4b3
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-17 11:28:16 +0200

    Update

--- E3.py
+++ E3.py
@@ -8,16 +8,16 @@ Mostra True si el cinquè valor, està comprès dins els dos rangs, si no False.
 '''
 #Els extrems dels rangs inclosos, el primer rang
 
-primervalor=int(input("1r enter: "))
-segonvalor=int(input("2n enter: "))
-tercervalor=int(input("3r enter: "))
-quartvalor=int(input("4t enter: "))
-cinquevalor=int(input("5è enter: "))
+primerValor=int(input("1r enter: "))
+segonValor=int(input("2n enter: "))
+tercerValor=int(input("3r enter: "))
+quartValor=int(input("4t enter: "))
+cinqueValor=int(input("5è enter: "))
 
-primrang=(primervalor <= cinquevalor <= segonvalor)
-segrang=(tercervalor <= cinquevalor <= quartvalor)
+primRang=(primerValor <= cinqueValor <= segonValor)
+segRang=(tercerValor <= cinqueValor <= quartValor)
 
-if primrang and segrang:
+if primRang and segRang:
     print("True")
 else:
     print("False")
