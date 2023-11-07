ffe45d9fe3e878adbb6a0677b9cee2c5b8b820ed
PolCerrilloITB <145445253+PolCerrilloITB@users.noreply.github.com>
2023-11-07 10:34:04 +0100

    Create e3.py

---
+++ e3.py
@@ -0,0 +1,9 @@
+'''
+Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
+7/1/2023
+ASIXc1C M03 UF1
+Descripció: Volem un programa que calculi la velocitat instantània i la velocitat mitjana.
+Cal demanar velocitat inicial (m/s), l'acceleració (m/s2) i el temps.
+Si la velocitat instantània és inferior o igual a 0, has d'indicar que està parat i no pots
+calcular la velocitat mitjana.
+'''
