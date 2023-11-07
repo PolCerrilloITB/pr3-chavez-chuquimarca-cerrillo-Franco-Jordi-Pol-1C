f523feeb7b05139af82bf13b3abcf3de329084e2
PolCerrilloITB <145445253+PolCerrilloITB@users.noreply.github.com>
2023-11-07 10:34:21 +0100

    Create e4.py

---
+++ e4.py
@@ -0,0 +1,8 @@
+'''
+Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
+7/1/2023
+ASIXc1C M03 UF1
+Descripció: Programa que demana l'import d'una factura, amb iva inclòs.
+Calcula l'import amb descompte, en cas de tenir la targeta de client,
+tenint en compte els següents criteris:
+'''
