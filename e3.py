a696d3099ca32df08f3ef1195dc72397f4f689a5
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-07 11:49:03 +0100

    update

--- e3.py
+++ e3.py
@@ -7,3 +7,12 @@ Cal demanar velocitat inicial (m/s), l'acceleració (m/s2) i el temps.
 Si la velocitat instantània és inferior o igual a 0, has d'indicar que està parat i no pots
 calcular la velocitat mitjana.
 '''
+velocitatInicial = int(input("Velocitat?"))
+acceleracio = float(input("Acceleració?"))
+temps = float(input("Temps?"))
+vI = velocitatInicial+acceleracio*temps
+vM = (velocitatInicial+vI)/2
+if velocitatInicial+acceleracio*temps == 0 or velocitatInicial+acceleracio*temps < 0:
+    print("El vehicle esta aturat o en direcció negativa i no put calcular la velocitat mitjana")
+else:
+    print("Velocitat inicial:", vI, "Velocitat mitjana:", vM)
