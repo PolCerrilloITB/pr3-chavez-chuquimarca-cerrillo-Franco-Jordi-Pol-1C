47afe9a754a3d7fbc191174ff5974be8c15202c7
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-07 11:31:46 +0100

    update

--- e2.py
+++ e2.py
@@ -4,3 +4,12 @@ Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 ASIXc1C M03 UF1
 Descripció: Programa que detecta si tres números demanats han estat introduïts en ordre creixent.
 '''
+
+num1, num2, num3 = (input("Tres numeros:").split())
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+if num1 < num2 and num2 < num3:
+    print("Ordre creixent")
+else:
+    print("Barreja")
