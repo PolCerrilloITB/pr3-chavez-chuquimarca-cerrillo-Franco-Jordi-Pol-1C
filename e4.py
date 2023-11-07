f4396a3f69f94e1312cb85db60da7eaf918d036e
franco.chavez.7e7 <franco.chavez.7e7@itb.cat>
2023-11-07 12:07:53 +0100

    final de clase 7 nov

--- e4.py
+++ e4.py
@@ -6,6 +6,12 @@ Descripció: Programa que demana l'import d'una factura, amb iva inclòs.
 Calcula l'import amb descompte, en cas de tenir la targeta de client,
 tenint en compte els següents criteris:
 '''
-weuifhweufhweugfhweuiog
+pTotal0 = int(input(print("Cual es el precio total de lo pagado, con el iva incluido?")))
 
-dd
+if pTotal0 >= 100:
+    pInicial = pTotal0*0.79
+    Descuento = pInicial*1.21
+
+    print("El precio con el descuento es de ",round(Descuento,2),"€")
+else:
+    print("No se aplica el descuento")
