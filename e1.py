9d9b338c87a2e8a0d659002805ccbeee59e0113f
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-07 11:22:03 +0100

    update

--- e1.py
+++ e1.py
@@ -5,3 +5,11 @@ ASIXc1C M03 UF1
 Descripcion: Programa que demana dos números si el primer és més gran o igual que el segon els intercanvia.
 I torna a mostrar els valors per pantalla
 """
+
+num1, num2= (input("Dos numeros? ").split())
+num1 = int(num1)
+num2 = int(num2)
+if num1 == num2 or num1 > num2:
+    print(num2, num1)
+else:
+    print(num1, num2)
