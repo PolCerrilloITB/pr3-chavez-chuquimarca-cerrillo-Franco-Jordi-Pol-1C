8a5283cd27dad719ed2124e971326e69b7542ae2
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-09 09:04:33 +0100

    update

--- e5.py
+++ e5.py
@@ -1,8 +1,31 @@
 '''
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
-7/1/2023
+9/1/2023
 ASIXc1C M03 UF1
 Descripció: Programa que comprovi si una data és correcte.
 El programa ha de demanar una data, dia, mes i any (en el format DD/MM/AAAA).
 Cal recordar que hi ha anys de traspàs
 '''
+dia, mes, any = input("Dia/mes/any: ").split("/")
+dia = int(dia)
+mes = int(mes)
+any = int(any)
+
+if mes == 1 | 3 | 5 | 7 | 8 | 10 | 12:
+    if dia > 0 and dia <= 31:
+        print("data correcte")
+elif mes == 4 | 6 | 9 | 11:
+    if dia > 0 and dia <= 31:
+        print("data correcte")
+elif mes == 2:
+    if any % 4 == 0:
+        if dia > 0 and dia <= 29:
+            print("data correcte")
+        else:
+            print("data incorrecte")
+    elif dia > 0 and dia <= 28:
+        print("data correcte")
+    else:
+        print("data incorrecte")
+else:
+    print("data incorrecte")
