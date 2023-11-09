661aa5060c4e6f6434e56e97dc36682ab9b7f4b0
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-09 09:41:11 +0100

    update

--- e6.py
+++ e6.py
@@ -5,9 +5,16 @@ ASIXc1C M03 UF1
 Descripció: L'usuari introdueix la lletra del tipus d'habitatge i número de m3  d'aigua gastats.
 Mostrar per pantalla el preu total de la factura de l’aigua. Arrodonit a 2 decimals
 '''
+
 tipo = input()
 m3 = float(input())
-m3 = round(m3,2)
+
+# Variables definides
+
+precioFijo = 0
+precio = 0
+
+# Tipus definits amb el seu preu definit
 
 if tipo == "A":
     precioFijo = 2.40
@@ -28,13 +35,15 @@ elif tipo == "H":
 elif tipo == "I":
     precioFijo = 61.31
 
-if m3 is [0-6]:
+# m3 definides amb els corresponents calculs
+
+if m3 == [0-6]:
     precio = m3*0.5849
-elif m3 is [7-9]:
+elif m3 == [7-9]:
     precio = m3*1.1699
-elif m3 is [10-15]:
+elif m3 == [10-15]:
     precio = m3*1.7548
-elif m3 is [16-18]:
+elif m3 == [16-18]:
     precio = m3*2.3397
 elif m3 > 18:
     precio = m3*2.9246
