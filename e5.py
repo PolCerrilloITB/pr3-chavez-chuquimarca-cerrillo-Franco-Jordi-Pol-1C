4d7baee6febcb576808237fbd7c9c8d5cdf2c081
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-09 09:17:50 +0100

    update

--- e5.py
+++ e5.py
@@ -6,10 +6,15 @@ Descripció: Programa que comprovi si una data és correcte.
 El programa ha de demanar una data, dia, mes i any (en el format DD/MM/AAAA).
 Cal recordar que hi ha anys de traspàs
 '''
+
+# Dia/mes/any els transformem a int i els fiquem a las seves variables
+
 dia, mes, any = input("Dia/mes/any: ").split("/")
 dia = int(dia)
 mes = int(mes)
 any = int(any)
+
+# Amb els if i els elif, fem la comprovació dels respectius dias mesos i anys.
 
 if mes == 1 | 3 | 5 | 7 | 8 | 10 | 12:
     if dia > 0 and dia <= 31:
