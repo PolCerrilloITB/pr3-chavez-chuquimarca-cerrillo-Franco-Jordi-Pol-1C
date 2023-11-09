2554b598261c123d7d98c2e7b0ca32fe69a25138
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-09 09:08:18 +0100

    update

--- e2.py
+++ e2.py
@@ -5,10 +5,18 @@ ASIXc1C M03 UF1
 Descripció: Programa que detecta si tres números demanats han estat introduïts en ordre creixent.
 '''
 
+# separació dels numeros amb el .split()
+
 num1, num2, num3 = (input("Tres numeros:").split())
+
+# pasar els numeros a int
+
 num1 = int(num1)
 num2 = int(num2)
 num3 = int(num3)
+
+# ordre dels numeros segons el seu valor
+
 if num1 < num2 and num2 < num3:
     print("Ordre creixent")
 else:
